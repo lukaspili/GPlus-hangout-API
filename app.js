6352d2a251de57f59793e05e3ac2478048c9feac
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-01-31 17:26:53 +0100

    Simplification from google sample.

--- app.js
+++ app.js
@@ -1,41 +1,9 @@
-(function() {
-  if (gapi && gapi.hangout) {
-
-    var initHangout = function(apiInitEvent) {
-      if (apiInitEvent.isApiReady) {
-
-
-        
-        // prepareAppDOM();
-
-        // gapi.hangout.data.onStateChanged.add(function(stateChangeEvent) {
-        //   updateLocalDataState(stateChangeEvent.state,
-        //                        stateChangeEvent.metadata);
-        // });
-        // gapi.hangout.onParticipantsChanged.add(function(partChangeEvent) {
-        //   updateLocalParticipantsData(partChangeEvent.participants);
-        // });
-
-        // if (!state_) {
-        //   var state = gapi.hangout.data.getState();
-        //   var metadata = gapi.hangout.data.getStateMetadata();
-        //   if (state && metadata) {
-        //     updateLocalDataState(state, metadata);
-        //   }
-        // }
-        // if (!participants_) {
-        //   var initParticipants = gapi.hangout.getParticipants();
-        //   if (initParticipants) {
-        //     updateLocalParticipantsData(initParticipants);
-        //   }
-        // }
-
-        gapi.hangout.layout.displayNotice('Hello World', true);
-
-        gapi.hangout.onApiReady.remove(initHangout);
-      }
-    };
-
-    gapi.hangout.onApiReady.add(initHangout);
+gapi.hangout.onApiReady.add(function(apiInitEvent) {
+  if (apiInitEvent.isApiReady) {
+    start();
   }
-})();
+});
+
+function start() {
+  gapi.hangout.layout.displayNotice('Hello World', true);
+}
