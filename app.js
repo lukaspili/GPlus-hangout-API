ffca4a55b22256b060b213124e869c1db52ba0d9
Lukasz Piliszczuk <lukasz.pili@gmail.com>
2012-01-31 18:34:31 +0100

    First js interaction with participants.

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+var users = new Array();
+
+var hands = new Array();
+
+var currentUser = null;
+
 gapi.hangout.onApiReady.add(function(apiInitEvent) {
   if (apiInitEvent.isApiReady) {
     start();
@@ -5,5 +11,50 @@ gapi.hangout.onApiReady.add(function(apiInitEvent) {
 });
 
 function start() {
-  gapi.hangout.layout.displayNotice('Hello World', true);
+
+  gapi.hangout.layout.displayNotice('Starting app...', false);
+
+  users = gapi.hangout.getParticipants();
+  currentUser = gapi.hangout.getParticipantById(gapi.hangout.getParticipantId());
+
+  refresh();
+
+  gapi.hangout.layout.displayNotice('Done !', false);
+}
+
+$('#button_ask').click(function() {
+
+  if($.inArray(currentUser, hands) > -1) {
+    gapi.hangout.layout.displayNotice('You already rised your hand', false);
+    return;
+  }
+
+  hands.push(currentUser);
+  refresh();
+
+  gapi.hangout.layout.displayNotice('You rised your hand', false);
+});
+
+$('#button_cancel').click(function() {
+
+  if($.inArray(currentUser, hands) == -1) {
+    gapi.hangout.layout.displayNotice('You didn\'t rise your hand', false);
+    return;
+  }
+
+  hands.remove(currentUser);
+  refresh();
+
+  gapi.hangout.layout.displayNotice('You hide your hand', false);
+});
+
+function refresh() {
+  
+  users.each(function(index) {
+    $('#users_list').append('<li>' + users[index].person.displayName + '</li>');
+  });
+
+  hands.each(function(index) {
+    $('#hands_list').append('<li>' + hands[index].person.displayName + '</li>');
+  });
 }
